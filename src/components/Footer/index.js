e69a1f22d77a4f32fc84205b0b9c781a95b760f5
willian <wopetri11@gmail.com>
2021-11-19 00:35:23 -0400

    Updated Header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center items-center h-12 w-full bg-primary text-secondary
     fixed bottom-0">
-      <p className='text-xl'>2021 Willian Petri</p>
+      <p className='text-xl'> Copyright &copy; 2021 Willian Petri</p>
     </div>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import github from '../../images/github.svg'
+import linkedin from '../../images/linkedin.svg'
 
 const Header = () => {
   return (
     <nav 
     className="flex justify-between items-center h-16 bg-primary text-secondary 
-    relative font-mono"
+    font-mono relative"
     role="navigation"
     >
       <Link to="/" className='pl-8'>
@@ -28,18 +30,20 @@ const Header = () => {
         </svg>
       </div> */}
       {/* colocar o hidden no className da div */}
-      <div className="pr-8 md-block ">
-        <Link to='/' className="p-4">
+      <div className="flex items-center">
+        <Link to='/' className="p-4 mr-3">
           Quem Sou
         </Link>
-        <Link to='/' className="p-4">
+        <Link to='/' className="p-4 mr-5">
           Projetos
         </Link>
-        <a href="https://github.com/willianPetri" className='p-4'>
-          Github
+        <a href='https://github.com/willianPetri' className="font-semibold inline-block py-3 mr-5 text-secondary">
+          <i className='fab fa-github text-lg leading-lg animate-bounce'/>
+          {/* <img src={github} alt="github" className='w-10 h-10 bg-secondary animate-bounce' /> */}
         </a>
-        <a href="https://www.linkedin.com/in/willian-petri-84a935135" className='p-4'>
-          LinkedIn
+        <a href='https://www.linkedin.com/in/willian-petri-84a935135/' className="font-semibold inline-block py-3 mr-5 text-secondary">
+        <i className='fab fa-linkedin text-lg leading-lg animate-bounce' />
+          {/* <img src={linkedin} alt="linkedin" className='w-10 h-10  animate-bounce' /> */}
         </a>        
       </div>
     </nav>
